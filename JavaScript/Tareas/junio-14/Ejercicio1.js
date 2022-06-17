fe17b31dcccf30759dfb05e1705e6a85f4162b19
Aileen Alvarado <monsealvarado775@gmail.com>
2022-06-17 13:39:48 -0500

    Subiento Tarea

---
+++ JavaScript/Tareas/junio-14/Ejercicio1.js
@@ -0,0 +1,7 @@
+ Complete the function to print out the string: This is a sentence.
+        
+let arr = ["This", "is", "a", "sentence."];
+function printOutString() {
+    arr.forEach(element => console.log(element));
+}
+printOutString();
---
+++ JavaScript/Tareas/junio-14/Ejercicio4.js
@@ -0,0 +1,13 @@
+Exercise #4
+
+Create a program that loops over the 2 arrays; 
+if there are any common courses print them out to the console.
+
+let student1Courses = ['Math', 'English', 'Programming'];
+let student2Courses = ['Geography', 'Spanish', 'Programming'];
+
+for(let i=0; i<student1Courses.length; i++){
+  if (student2Courses.includes(student1Courses[i])){
+    console.log("Encontrado "+ student1Courses[i])
+  }    
+} 
