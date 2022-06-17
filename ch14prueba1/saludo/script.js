df5276732ecc152aa81ecd229a98e785b3e84db6
Aileen Alvarado <monsealvarado775@gmail.com>
2022-06-17 18:02:32 -0500

    subir ejercicios

--- ch14prueba1/saludo/script.js
+++ ch14prueba1/saludo/script.js
@@ -1,1 +1,18 @@
 console.log("se nota que todavia me extrañas ");
+
+setInterval(cambiarSaludo, 2000);
+
+function cambiarSaludo() {
+    let elementoh2 =document.getElementsByTagName("h2")[0];
+    let ganando = "Ganando como siempre!"
+    let moda = "Impongo moda para las niñas";
+
+    let frase = elementoh2.innerHTML
+    console.log(frase);
+    if (elementoh2.innerHTML == ganando) {
+        elementoh2.innerHTML = moda;
+    } else {
+        elementoh2.innerHTML =  ganando;
+    }
+    console.log("frase " + frase);
+}
